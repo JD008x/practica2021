a1cc22b925978f6922f38d50a7213c364df93db0
Rares Catalin Onescu <raresonescu@gmail.com>
2021-09-07 10:46:40 +0300

    modificare din romana in eng

--- src/models/location.model.ts
+++ src/models/location.model.ts
@@ -4,9 +4,9 @@ import ILocation from '../interfaces/ILocation';
 const LocationSchema: mongoose.Schema = new mongoose.Schema(
     {
         id: { type: Number, required: true},
-        nume: { type: String, required: true},
-        adresa: { type: String, required: true},
-        nrTelefon: { type: Date }
+        name: { type: String, required: true},
+        address: { type: String, required: true},
+        phoneNumber: { type: Date }
     }
 )
 

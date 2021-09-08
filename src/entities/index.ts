75f644b1cb83309eee9205b990f31a6ec5a24656
Vlad1111 <sbengheci.vlad@yahoo.com>
2021-09-08 17:04:16 +0300

    finale push

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,5 @@
+import { Location } from "./location.entity";
 import { User } from "./user.entity";
-import { Category } from "./category.entity";
+
 // export "entities" array to be used with "MikroORM.init"
-export default [ User, Category];
+export default [ User, Location ];

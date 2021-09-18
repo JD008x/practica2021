37a2bcbb3d304da0b7a29b952bcf10eedad2a9a0
Sirbu Alexandru-Paul <justst4r@gmail.com>
2021-09-18 22:06:21 +0300

    creare componenta contact

--- InventoryAppAngular/src/app/app-routing.module.ts
+++ InventoryAppAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'header',component: HeaderComponent },
+  { path: '**', redirectTo: '/header' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

53fbd86695bee21d47f5f801e335dbc5d6bbb08b
Vlad1111 <sbengheci.vlad@yahoo.com>
2021-09-20 18:47:12 +0300

    finish refactoring css + refactoring environment variables

--- InventoryAppAngular/src/app/services/itemServices.ts
+++ InventoryAppAngular/src/app/services/itemServices.ts
@@ -1,46 +1,45 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 import { Item } from "../models/item";
 
 
 @Injectable()
 export class ItemServices {
 
-  baseAdrress: string = `http://localhost:80`;
-
   constructor(private httpClient: HttpClient,
   ) { }
 
   getItems(): Observable<Item[]> {
-    const url = `http://localhost:80`;
+    const url = environment.baseAdrress;
     return this.httpClient.get(url) as Observable<Item[]>;
 
   }
 
   getItemByInventoryNumber(inventoryNumber: string): Observable<Item> {
-    const url = `http://localhost:4200/${inventoryNumber}`;
+    const url = `${environment.baseAdrress}/${inventoryNumber}`;
     return this.httpClient.get(url) as Observable<Item>;
   }
 
 
   getItemById(id: number): Observable<Item> {
-    const url = `http://localhost:4200//id/${id}`;
+    const url = `${environment.baseAdrress}/id/${id}`;
     return this.httpClient.get(url) as Observable<Item>;
   }
 
   addItem(object: Item): Observable<Item> {
-    const url = `http://localhost:4200`;
+    const url = environment.baseAdrress;
     return this.httpClient.post(url, object) as Observable<Item>;
   }
 
   editItem(object: Item): Observable<Item> {
-    const url = `http://localhost:4200`;
+    const url = environment.baseAdrress;
     return this.httpClient.put(url, object) as Observable<Item>;
   }
 
   deleteItem(id: number): Observable<null> {
-    const url = `http://localhost:4200/${id}`;
+    const url = `${environment.baseAdrress}/${id}`;
     return this.httpClient.delete(url) as unknown as Observable<null>;
   }
 }

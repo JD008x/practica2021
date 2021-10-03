ad8cdb1c209dd996697125e75ec6d53a2742f8cd
Sirbu Alexandru-Paul <justst4r@gmail.com>
2021-10-03 18:47:24 +0300

    added location list and refactoring html

--- InventoryAppAngular/src/app/components/contact/contact.component.ts
+++ InventoryAppAngular/src/app/components/contact/contact.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { LocationServices } from 'src/app/services/locationServices';
+import { Location } from 'src/app/models/location';
+
 
 @Component({
   selector: 'app-contact',
@@ -9,29 +13,77 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class ContactComponent implements OnInit {
 
   contactFormGroup: FormGroup;
+  location: Location[] = this.locationService.locationList;
+  dataSources: MatTableDataSource<Location> = new MatTableDataSource<Location>(this.location);
+  parent!: Location;
+  loading = false;
+  buttonText = "Submit";
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder,private locationService: LocationServices) { 
     this.contactFormGroup = Object();
   }
 
   ngOnInit(): void { 
-    
+    this.getCategoryList();
+
     this.contactFormGroup = this.fb.group({
-    name: [ null, Validators.required],
-    email: [null, Validators.maxLength(100)],
+    email: [null, [Validators.email, Validators.required]],
     message: [ null, Validators.required]
 
    })
   }
-   
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  locationColumns: string[] = [
+    'name',
+    'address',
+    'telNumber'
+  ];
+
+  getCategoryList(): void {
+    this.locationService.getLocations().subscribe((list: Location[]) => {
+      this.location = list;
+      this.updateTable();
+    }, (err) => {
+      if (err.status === 401) return;
+    });
+  }
+
+  updateTable(): void {
+    this.dataSources = new MatTableDataSource<Location>(this.location);
+  }
+
+  hasError(controlName: string, errorName: string) {
+    return this.contactFormGroup.controls[controlName].hasError(errorName);
+  }
 
   onSubmit()
-  {
-    
-  }
+  {}
+  // onSubmit()
+  // {
+  //   this.loading = true;
+  //   this.buttonText = "Submiting...";
+
+  //   let user = {
+  //     email: this.contactFormGroup.value.email,
+  //     message: this.contactFormGroup.value.message
+  //   }
+  //   this.http.sendEmail( user ).subscribe(
+  //     data => {
+  //       let res:any = data; 
+  //       console.log(
+  //         `👏 > 👏 > 👏 > 👏 ${user.email} is successfully register and mail has been sent and the message id is ${res.messageId}`
+  //       );
+  //     },
+  //     err => {
+  //       console.log(err);
+  //       this.loading = false;
+  //       this.buttonText = "Submit";
+  //     },() => {
+  //       this.loading = false;
+  //       this.buttonText = "Submit";
+  //     }
+  //   );
+  // }
+
+
 }

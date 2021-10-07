05ef1e54b83c886c0cca4c59876a5405b006313e
Lorena Popa <L.Popa@peoplepower.ro>
2021-10-07 22:18:44 +0300

    debug

--- InventoryAppAngular/src/app/components/view-item/view-item.component.ts
+++ InventoryAppAngular/src/app/components/view-item/view-item.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Item } from 'src/app/models/item';
 import { ItemServices } from 'src/app/services/itemServices';
-import {MatDialog, MatDialogConfig } from '@angular/material/dialog'
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
 import { MyDialogComponent } from '../my-dialog/my-dialog.component';
 
 @Component({
@@ -19,22 +19,21 @@ export class ViewItemComponent implements OnInit {
   qrValue!: string;
   qrsize: number = 200;
   constructor(private itemService: ItemServices, private activatedRoute: ActivatedRoute,
-    private router: Router,private dialog: MatDialog) {
-
+    private router: Router, private dialog: MatDialog) {
     this.activatedRoute.params.subscribe((params) => {
       this.itemId = params.id;
       this.item = new Item();
     });
     this.qrValue = this.itemId.toString();
   }
-  
+
   onClickEditItem(): void {
 
-    this.router.navigate(['/edit/'+this.itemId]);
+    this.router.navigate(['/edit/' + this.itemId]);
   }
   onClickViewQr(): void {
     const dialogRef = this.dialog.open(MyDialogComponent, {
-    data:this.qrValue
+      data: this.qrValue
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -42,7 +41,24 @@ export class ViewItemComponent implements OnInit {
     });
   }
 
+  ngonchanges(): void {
+
+    if (!this.itemId) {
+      this.item = new Item();
+    } else {
+      this.itemService.getItemById(this.itemId).subscribe({
+
+        next: item => {
+          this.item = new Item(item);
+          this.itemIsFound = this.item.id === '' ? false : true;
+        }
+      });
+    }
+
+  }
+
   ngOnInit(): void {
+
     if (!this.itemId) {
       this.item = new Item();
     } else {
